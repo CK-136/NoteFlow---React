f990e0dc8099999cd3de163f3c5ac79a80b267a9
chriss <chrisscolin13@gmail.com>
2025-11-14 14:26:00 +0200

    page router

--- NoteFlow/src/App.jsx
+++ NoteFlow/src/App.jsx
@@ -1,11 +1,23 @@
+import { Routes, Route, Link } from "react-router-dom";
+import Landing from "./pages/Landing";
+import About from "./pages/About";
+import Blog from "./pages/Blog";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
+      <nav>
+        <Link to='/'>Home</Link>
+        <Link to='/about'>About</Link>
+        <Link to='/blog'>Blog</Link>
+      </nav>
+      <Routes>
+        <Route path='/' element={<Landing />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/blog' element={<Blog />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
